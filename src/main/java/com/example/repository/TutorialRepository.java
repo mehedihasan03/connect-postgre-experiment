e0535eb45dcd915872ca87d923abb142c3e74e28
mehedihasan03 <mdmehedi03@gmail.com>
2022-12-12 13:32:44 +0600

    first commit

---
+++ src/main/java/com/example/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.example.repository;
+
+import com.example.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
+}
